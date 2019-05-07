effb6418d5e13bd4212ec86c4655cc187d364962
Laykel <layke.luc@gmail.com>
2019-05-07 20:26:19 +0200

    Finished up Musician program

--- src/auditor.js
+++ src/auditor.js
@@ -1,6 +1,17 @@
+// Auditor program: receives music through given protocol (udp) and
+// sends a list of active musicians to its client (tcp).
+// Usage: node auditor.js
+
 const dgram = require('dgram');
 
 const protocol = require('./orchestra-protocol');
 
 const socket = dgram.createSocket('udp6');
+socket.bind(protocol.PROTOCOL_PORT, () => {
+  console.log('Joining multicast group');
+  socket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
+});
 
+socket.on('message', (msg, source) => {
+  console.log(`Data has arrived: ${msg}. Source port: ${source.port}.`);
+});
--- src/musician.js
+++ src/musician.js
@@ -1,15 +1,61 @@
-// Simple UDP server
+// Musician program: sends music through given protocol
+// Usage node musician.js instrument
+// instrument := piano | trumpet | flute | violin | drum
 
+// UDP datagram (core node package)
 const dgram = require('dgram');
-
-const protocol = require('./orchestra-protocol.js');
-
+// Generate random uuid for musician
+const uuid = require('uuid/v4');
+// Our own protocol definition
+const protocol = require('./orchestra-protocol');
+// Create a socket to send music
 const socket = dgram.createSocket('udp6');
-socket.bind(protocol.PROTOCOL_PORT, () => {
-  console.log('Joining multicast group');
-  socket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
-});
 
-socket.on('message', (msg, source) => {
-  console.log(`Data has arrived: ${msg}. Source port: ${source.port}.`);
-});
+// Map which sound is made by which instrument
+const sound = {
+  piano: 'ti-ta-ti',
+  trumpet: 'pouet',
+  flute: 'trulu',
+  violin: 'gzi-gzi',
+  drum: 'boum-boum',
+};
+
+// Main function
+class Musician {
+  // Musician constructor
+  constructor(instrument) {
+    this.musician = uuid();
+    this.instrument = instrument;
+    this.music = sound[instrument];
+  }
+
+  // Function that sends payload
+  play() {
+    const music = {
+      musician: this.musician,
+      instrument: this.instrument,
+      sound: this.music,
+    };
+
+    const message = JSON.stringify(music);
+
+    const payload = Buffer.from(message);
+    // Send payload
+    socket.send(payload, 0, payload.length, protocol.PROTOCOL_PORT,
+      protocol.PROTOCOL_MULTICAST_ADDRESS, () => {
+        console.log(`Sending payload: ${payload} via port ${socket.address().port}.`);
+      });
+  }
+}
+
+// Get instrument from command line argument
+if (process.argv.length !== 3 || !(process.argv[2] in sound)) {
+  throw new Error('Usage: node musician.js <instrument>');
+}
+const instrument = process.argv[2];
+
+// Create musician
+const musician = new Musician(instrument);
+
+// Send music every second
+setInterval(musician.play.bind(musician), 1000);
